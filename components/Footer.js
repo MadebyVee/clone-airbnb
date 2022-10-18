676ea0b1b0cbd1fb0098f8b5f31d7362326d1654
MadebyVee <Davillefactory@gmail.com>
2022-10-18 18:27:13 -0400

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 bg-gray-100 text-gray-600 py-14">
+            
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>ABOUT</h5>
+            <p>How React works</p>
+            <p>Portfolio</p>
+            <p>NextJs</p>
+            <p>TailWind</p>
+            <p>Since 2001</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>COMMUNITY</h5>
+            <p>How React works</p>
+            <p>Portfolio</p>
+            <p>NextJs</p>
+            <p>TailWind</p>
+            <p>Since 2001</p>
+            </div>
+           
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>HOST</h5>
+            <p>How React works</p>
+            <p>Portfolio</p>
+            <p>NextJs</p>
+            <p>TailWind</p>
+            <p>Since 2001</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5>SUPPORT</h5>
+            <p>How React works</p>
+            <p>Portfolio</p>
+            <p>NextJs</p>
+            <p>TailWind</p>
+            <p>Since 2001</p>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Footer;
